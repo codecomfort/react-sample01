75d1f9b68aca0db010c70db48c51774ea4facad6
Hiroyuki Naito <hiroyuki.naitou@gmail.com>
2016-09-12 02:56:20 +0900

    Refactor Menuを別ファイルへ分離

--- sample01/src/App.js
+++ sample01/src/App.js
@@ -1,31 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import './css/side-menu.css';
 import MenuLink from './components/MenuLink'
-
-
-class Menu extends Component {
-    render() {
-      return (
-        <div id="menu">
-            <div className="pure-menu">
-                <a className="pure-menu-heading" href="#">Company</a>
-
-                <ul className="pure-menu-list">
-                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">About</a></li>
-
-                    <li className="pure-menu-item" className="menu-item-divided pure-menu-selected">
-                        <a href="#" className="pure-menu-link">Services</a>
-                    </li>
-
-                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Contact</a></li>
-                </ul>
-            </div>
-        </div>
-      )
-    }
-}
-
+import Menu from './components/Menu'
 
 class Main extends Component {
   static propTypes = {
