5b8885e7c85525d0241437e271aa2082f5116142
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2016-09-12 16:03:16 +0900

    Fix 軽微なバグを修正

--- sample01/src/App.js
+++ sample01/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     const SAMPLE_DATA = [
       { id: 0, src: "http://farm3.staticflickr.com/2875/9069037713_1752f5daeb.jpg", alt: "Peyto Lake"},
-      { id: 1, src: "http://farm3.staticflickr.com/2813/9069585985_80da8db54f.jpg", "alt": "Train"},
+      { id: 1, src: "http://farm3.staticflickr.com/2813/9069585985_80da8db54f.jpg", alt: "Train"},
       { id: 2, src: "http://farm6.staticflickr.com/5456/9121446012_c1640e42d0.jpg", alt: "T-Shirt Store"},
       { id: 3, src: "http://farm8.staticflickr.com/7357/9086701425_fda3024927.jpg", alt: "Mountain"}
     ]
--- sample01/src/components/Menu.js
+++ sample01/src/components/Menu.js
@@ -8,14 +8,18 @@ class Menu extends Component {
                 <a className="pure-menu-heading" href="#">Company</a>
 
                 <ul className="pure-menu-list">
-                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">About</a></li>
-
-                    <li className="pure-menu-item" className="menu-item-divided pure-menu-selected">
+                    <li className="pure-menu-item">
+                      <a href="#" className="pure-menu-link">Home</a>
+                    </li>
+                    <li className="pure-menu-item">
+                      <a href="#" className="pure-menu-link">About</a>
+                    </li>
+                    <li className="pure-menu-item menu-item-divided pure-menu-selected">
                         <a href="#" className="pure-menu-link">Services</a>
                     </li>
-
-                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Contact</a></li>
+                    <li className="pure-menu-item">
+                      <a href="#" className="pure-menu-link">Contact</a>
+                    </li>
                 </ul>
             </div>
         </div>
